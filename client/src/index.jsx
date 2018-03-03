56bd15fb3a8c5e95b1859322c156b321e17676c3
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-03-02 16:14:15 -0800

    Get restaurant iterator from page url

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,10 @@ import App from './components/App.jsx';
 
 // ID # from proxy server
 // workable ids: 1, 5, 12, 13, 15, 17 ...
-var iterator = 1;
+// var iterator = 5;
+
+// get restaurant iterator from the page URL
+let iterator = window.location.href.split("/").pop().substring(4) || '1';
 
 ReactDOM.render(
 	<App iterator={ iterator } />,
