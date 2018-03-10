5899a05e67b7a6e84be6c626727d5ff79fae6762
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-03-09 16:33:13 -0800

    Connect to AWS RDS database

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,23 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const dbconfig = require('../../opsworks.js');
+
+// var db = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '' // TODO: Update with your MySQL password
+// });
 
 var db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '' // TODO: Update with your MySQL password
+	host: dbconfig.host,
+	user: dbconfig.user,
+	password: dbconfig.password,
+	port: dbconfig.port,
+	database: dbconfig.database
 });
 
 db.connect(err => {
 	if (err) throw err;
-	const dbName = 'open_source_table_reviews';
-	console.log('connected to mysql db and using db', dbName);
-	db.query('use ' + dbName);
+	console.log('connected to mysql db', dbconfig.database);
 });
 
 module.exports = db;
